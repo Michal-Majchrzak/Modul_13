749c08e02f6b05e18036350fca48bbfc8971a4ee
Michał <mmajchrzak1@gmail.com>
2021-03-10 18:47:10 +0100

    Add configuration from instance folder

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from views import views
 from api import api
+import os
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'somesecret'
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('config.py', silent=True)
+
+try:
+    os.makedirs(app.instance_path)
+except OSError as error:
+    print(error)
 
 app.register_blueprint(views.general)
 app.register_blueprint(api.api)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
