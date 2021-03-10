96a51fb924b733f47ce993bbdc463d859e0c18eb
Michał <mmajchrzak1@gmail.com>
2021-03-10 17:27:24 +0100

    Update app.py
    Add 2nd Blueprint and move error responses to API

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask
 from views import views
+from api import api
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'somesecret'
 
 app.register_blueprint(views.general)
-
-
-@app.errorhandler(400)
-def bad_request(error):
-    return make_response(jsonify({'error': 'Bad request', 'status_code': 400}), 400)
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found', 'status_code': 404}), 404)
+app.register_blueprint(api.api)
 
 
 if __name__ == '__main__':
