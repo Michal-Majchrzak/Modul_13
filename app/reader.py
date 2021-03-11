6ebbd229d51a933b51368aa2fe7a84cd0291fba7
Michał <mmajchrzak1@gmail.com>
2021-03-11 12:16:15 +0100

    Add readers.py

---
+++ app/reader.py
@@ -0,0 +1,10 @@
+from flask import render_template, Blueprint
+from app.models import Reader
+
+readers = Blueprint('readers', __name__, url_prefix='/readers')
+
+
+@readers.route('/list', methods=['GET'])
+def render_readers_list():
+    readers_list = Reader.query.all()
+    return render_template('/reader/reader.html', readers_list=readers_list)
