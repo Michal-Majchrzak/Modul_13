61bd172476d49cfd65fb7eb1c5bde933a8897d5d
Michał <mmajchrzak1@gmail.com>
2021-03-11 10:10:18 +0100

    Update booklibrary
    Added Reader and InvenotryUnit to shell context manager

--- booklibrary.py
+++ booklibrary.py
@@ -1,11 +1,13 @@
 from app import app, db
-from app.models import Book, Author
+from app.models import Book, Author, Reader, InventoryUnit
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {
-       "db": db,
-       "Book": Book,
-       "Author": Author
+        "db": db,
+        "Book": Book,
+        "Author": Author,
+        "Reader": Reader,
+        "InventoryUnit": InventoryUnit
     }
